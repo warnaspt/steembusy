736d81705c70fded6265a34547fcc8f357f83529
warnaspt <34672601+warnaspt@users.noreply.github.com>
2018-09-15 23:44:58 +0100

    Updated website name

--- src/client/user/User.js
+++ src/client/user/User.js
@@ -127,7 +127,7 @@ export default class User extends React.Component {
     const url = `${busyHost}/@${username}`;
     const displayedUsername = profile.name || username || '';
     const hasCover = !!profile.cover_image;
-    const title = `${displayedUsername} - Busy`;
+    const title = `${displayedUsername} - SteemPT`;
 
     const isSameUser = authenticated && authenticatedUser.name === username;
 
@@ -143,7 +143,7 @@ export default class User extends React.Component {
           <meta property="og:url" content={url} />
           <meta property="og:image" content={image} />
           <meta property="og:description" content={desc} />
-          <meta property="og:site_name" content="Busy" />
+          <meta property="og:site_name" content="SteemPT" />
 
           <meta property="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
           <meta property="twitter:site" content={'@steemit'} />
