8cf77024a2b5d3e85d10de053b2649ae7c9474cf
warnaspt <34672601+warnaspt@users.noreply.github.com>
2018-09-29 16:31:01 +0100

    Added Telegram.js to RightSidebar

--- src/client/app/Sidebar/RightSidebar.js
+++ src/client/app/Sidebar/RightSidebar.js
@@ -19,6 +19,7 @@ import Loading from '../../components/Icon/Loading';
 import UserActivitySearch from '../../activity/UserActivitySearch';
 import WalletSidebar from '../../components/Sidebar/WalletSidebar';
 import FeedSidebar from '../../components/Sidebar/FeedSidebar';
+import telegram from '../../app/Sidebar/telegram.js';
 
 @withRouter
 @connect(
